ad652824d28477fcc72baff3a3e3122be67a97e7
ktu23 <89479438+ktu23@users.noreply.github.com>
2021-10-13 04:14:20 -0700

    Revert "Updates for Tic Tac Toe Project - Kevin Tu"

--- app/app.js
+++ app/app.js
@@ -1,29 +1,9 @@
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
-const authEvents = require('./auth/events')
-
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
 $(() => {
-  // Whenever our #sign-up form is submitted, call the `onSignUp` function
-  $('#sign-up').on('submit', authEvents.onSignUp)
-  $('#sign-in').on('submit', authEvents.onSignIn)
-  // select the button with the id sign out and call the onSightOut
-  // function anytime the button is clicked
-  $('#sign-out').on('submit', authEvents.onSignOut)
-  $('#new-game').on('submit', authEvents.onNewGame)
-
-  // $('#change-password').on('submit', authEvents.onChangePassword)
-
-  $('.play1').on('click', authEvents.onClick)
-  $('.play2').on('click', authEvents.onClick)
-  $('.play3').on('click', authEvents.onClick)
-  $('.play4').on('click', authEvents.onClick)
-  $('.play5').on('click', authEvents.onClick)
-  $('.play6').on('click', authEvents.onClick)
-  $('.play7').on('click', authEvents.onClick)
-  $('.play8').on('click', authEvents.onClick)
-  $('.play9').on('click', authEvents.onClick)
+  // your JS code goes here
 })
